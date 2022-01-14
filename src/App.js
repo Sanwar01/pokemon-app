6776bdcffbd606c90a5e1bdb12ba9180153122a5
Sanwar10 <sulemananwar99@gmail.com>
2022-01-14 15:28:45 +0000

    added previous and next buttons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ const App = () => {
   const { data, loading, error } = useQuery(GET_POKEMON_INFO, {
     variables: { first: 1 },
   });
+
   console.log(data);
+
+  const previousPokemon = () => {};
+  const nextPokemon = () => {};
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
@@ -18,10 +23,17 @@ const App = () => {
       </header>
 
       <div className="pokemon-container">
+        <button className="previous-pokemon-btn" onClick={previousPokemon}>
+          &#8249;
+        </button>
+
         {data &&
           data.pokemons.map((pokemon) => (
             <PokemonCard key={pokemon.id} pokemon={pokemon} />
           ))}
+        <button className="next-pokemon-btn" onClick={nextPokemon}>
+          &#8250;
+        </button>
       </div>
     </div>
   );
